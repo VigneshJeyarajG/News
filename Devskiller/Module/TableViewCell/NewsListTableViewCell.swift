32758e906ffebb3164d3a82be8429c0e3892ae64
Vignesh <vignesh.vamosys@gmail.com>
2022-07-26 18:53:49 +0530

    first commit

---
+++ Devskiller/Module/TableViewCell/NewsListTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  NewsListTableViewCell.swift
+//  Devskiller
+//
+//  Created by Vamosys on 26/07/22.
+//  Copyright © 2022 Mindera. All rights reserved.
+//
+
+import UIKit
+
+class NewsListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
